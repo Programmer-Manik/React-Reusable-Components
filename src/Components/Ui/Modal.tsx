b8b70760f495c0cc6557fdbab3b8c1f69336a758
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-01-15 00:16:46 +0600

    type script add and error solve any error

--- src/Components/Ui/Modal.tsx
+++ src/Components/Ui/Modal.tsx
@@ -1,11 +1,29 @@
 // import React from 'react';
 import { createPortal } from "react-dom";
 import cn from "../../Utils/cn";
-import {  MouseEvent, createContext, useContext, useRef } from "react";
+import {  MouseEvent, ReactNode, createContext, useContext, useRef } from "react";
 
-const ModalContext = createContext(null);
 
-const Modal = ({isOpen, onClose, children}) => {
+
+const ModalContext = createContext<TModalContext  | null>(null);
+
+type TModalContext = {
+   onClose : () => void;
+}
+
+type TModal = {
+   isOpen: boolean;
+   onClose: () => void;
+   children: ReactNode;
+};
+
+type TClose = {
+   children ?: ReactNode;
+}
+
+type THeader = TClose
+
+const Modal = ({isOpen, onClose, children}:TModal) => {
   
    const containerRef = useRef<HTMLDivElement>(null);
   
@@ -23,7 +41,7 @@ const Modal = ({isOpen, onClose, children}) => {
             })}
                onClick={handelOutsideClose}
             >
-            <div ref={containerRef} className="rounded-md bg-white w-full max-w-sm">
+            <div ref={containerRef} className="rounded-md p-3 bg-white w-full max-w-sm">
                {children}
             </div>
          </div>
@@ -34,19 +52,19 @@ const Modal = ({isOpen, onClose, children}) => {
 };
 
 
-const CloseButton = ({children}) => {
-   const {onClose} = useContext(ModalContext)
+const CloseButton = ({children}: TClose) => {
+   const {onClose} = useContext(ModalContext) as TModalContext
    return <button onClick={onClose}>
      {children ? (children): (
-       <svg className="size-5 bg-red-400 text-white p-0.5 rounded-md" fill="none" strokeWidth={4} stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+       <svg className="size-5 bg-red-400 text-white  p-0.5 rounded-md" fill="none" strokeWidth={4} stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
     </svg>
      )}
  </button>
 };
 
-const Header = ({children}) => {
-   return <div className="flex  justify-end">{children}</div>
+const Header = ({children}:THeader) => {
+   return <div className="flex  justify-end mb-1">{children}</div>
 }
 
 Modal.Header = Header;
