1b948a4d3798e6c932f60bb4f977830ed17ad8d9
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-01-14 20:14:15 +0600

    project stard next time

--- src/Components/Ui/Button.tsx
+++ src/Components/Ui/Button.tsx
@@ -1,13 +1,13 @@
 import cn from "../../Utils/cn"
 
 const Button = ({className, outline}) => {
-   return (
-      <button className={cn(
-         'bg-purple-500 px-3 py-2 required-md',{
-            'border border-purple-500 bg-opacity-10': outline,
-         },
-         className
-      )}
+   // return (
+   //    <button className={cn(
+   //       'bg-purple-500 px-3 py-2 required-md',{
+   //          'border border-purple-500 bg-opacity-10': outline,
+   //       },
+   //       className
+   //    )}
       >
          click
       </button>
