c68821a553cb10b2dd3bc6a4f4e72a815737bbd4
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-01-15 21:29:05 +0600

    problems solve normalfuntion

--- src/Components/NormalForm/NormalForm.tsx
+++ src/Components/NormalForm/NormalForm.tsx
@@ -1,15 +1,16 @@
-import {useState} from 'react'
-import { useForm } from 'react-hook-form';
+// import {useState} from 'react'
+import { useForm } from "react-hook-form";
 
-const {register, handleSubmit} = useForm()
-
-const onSubmit = (data: any) => {
-    console.log(data)
+const onSubmitData = (data: any) => {
+   console.log(data)
 }
 
 const NormalForm = () => {
+
+   const {register,handleSubmit} = useForm();
+   
    return (
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmitData)}>
          <label htmlFor='name'>name</label>
          <input type="text"  id="name" {...register('name')} />
          <button type="submit">submit</button>
