8f950998918b9536cc8f88ddd80078eda2143336
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-01-15 21:48:26 +0600

    form handleSubmit

--- src/Components/NormalForm/NormalForm.tsx
+++ src/Components/NormalForm/NormalForm.tsx
@@ -1,21 +1,32 @@
 // import {useState} from 'react'
 import { useForm } from "react-hook-form";
 
-const onSubmitData = (data: any) => {
-   console.log(data)
-}
-
 const NormalForm = () => {
+  const { register, handleSubmit } = useForm();
 
-   const {register,handleSubmit} = useForm();
-   
-   return (
-      <form onSubmit={handleSubmit(onSubmitData)}>
-         <label htmlFor='name'>name</label>
-         <input type="text"  id="name" {...register('name')} />
-         <button type="submit">submit</button>
-      </form>
-   );
+  const onSubmitData = (data: any) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmitData)} className="border border-red-400 max-w-5xl p-5 mx-auto">
+      <div className=" border p-4 border-purple-400 gap-5 grid grid-cols-2">
+        <div className="w-full">
+          <label className="block" htmlFor="name">name</label>
+          <input className="w-full" type="text" id="name" {...register("name")} />
+        </div>
+        <div className="w-full">
+          <label className="block" htmlFor="name">email</label>
+          <input className="w-full" type="text" id="name" {...register("name")} />
+        </div>
+        <div className="w-full">
+          <label className="block" htmlFor="name">password</label>
+          <input className="w-full" type="text" id="name" {...register("name")} />
+        </div>
+      </div>
+      {/* <button type="submit">submit</button> */}
+    </form>
+  );
 };
 
 export default NormalForm;
