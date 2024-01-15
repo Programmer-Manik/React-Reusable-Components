cc7c51d9f1011edd8bd00f1276a31576c09a7760
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-01-15 23:39:01 +0600

    form validation

--- src/Components/NormalForm/NormalForm.tsx
+++ src/Components/NormalForm/NormalForm.tsx
@@ -1,6 +1,7 @@
 // import {useState} from 'react'
 import { useForm } from "react-hook-form";
 import cn from "../../Utils/cn";
+import Button from "../Ui/Button";
 
 const NormalForm = () => {
   const { register, handleSubmit } = useForm();
@@ -9,46 +10,87 @@ const NormalForm = () => {
     console.log(data);
   };
 
-  const double = true ;
+  const double = true;
 
   return (
-    <form onSubmit={handleSubmit(onSubmitData)} className={cn("border border-red-400  p-5 mx-auto",{'max-w-5xl':double,
-      'max-w-md': !double,
-  })}>
-      <div className={cn(" border  border-blue-400 gap-5 grid grid-cols-1  justify-items-center",{
-         'md:grid-cols-2' : double,        
-      })}>
+    <form
+      onSubmit={handleSubmit(onSubmitData)}
+      className={cn("border border-red-300 rounded-lg shadow-sm  p-5 mx-auto", {
+        "max-w-5xl": double,
+        "max-w-md": !double,
+      })}
+    >
+      <div
+        className={cn(" gap-5 grid grid-cols-1  justify-items-center", {
+          "md:grid-cols-2": double,
+        })}
+      >
         <div className="w-full max-w-md">
-          <label className="block" htmlFor="name">name</label>
-          <input className="w-full" type="text" id="name" {...register("name")} />
+          <label className="block" htmlFor="name">
+            name
+          </label>
+          <input
+            className="w-full"
+            type="text"
+            id="name"
+            {...register("name")}
+          />
         </div>
         <div className="w-full max-w-md">
-          <label className="block" htmlFor="name">email</label>
-          <input className="w-full" type="text" id="name" {...register("name")} />
+          <label className="block" htmlFor="name">
+            email
+          </label>
+          <input
+            className="w-full"
+            type="text"
+            id="name"
+            {...register("name")}
+          />
         </div>
         <div className="w-full max-w-md">
-          <label className="block" htmlFor="name">password</label>
-          <input className="w-full" type="text" id="name" {...register("name")} />
+          <label className="block" htmlFor="name">
+            password
+          </label>
+          <input
+            className="w-full"
+            type="text"
+            id="name"
+            {...register("name")}
+          />
         </div>
         <div className="w-full max-w-md">
-        <label className="block" htmlFor="name">Select</label>
-         <select>
-               <option>one</option>
-               <option>tow</option>
-               <option>three</option>
-               <option>four</option>
-            </select>
+          <label className="block" htmlFor="name">
+            Select
+          </label>
+          <select>
+            <option>one</option>
+            <option>tow</option>
+            <option>three</option>
+            <option>four</option>
+          </select>
         </div>
         <div className="w-full max-w-md">
-        <label className="block" htmlFor="name">textarea</label>
-            <textarea></textarea>
+          <label className="block" htmlFor="name">
+            textarea
+          </label>
+          <textarea></textarea>
         </div>
         <div className="w-full max-w-md">
-            <label className="block" htmlFor="name">checkbox</label>
-            <input type="checkbox" name="" id="" />
+          <label className="block" htmlFor="name">
+            checkbox
+          </label>
+          <input type="checkbox" name="" id="" />
         </div>
       </div>
-      {/* <button type="submit">submit</button> */}
+      <div
+          className={cn("grid grid-cols-1 justify-items-center gap-5 my-8 ", {
+            "md:grid-cols-2": double,
+          })}
+        >
+          <div className="w-full max-w-md col-start-1 md:col-start-2 flex justify-end">
+             <Button className="w-full md:w-fit" type="submit">submit</Button >
+          </div>
+        </div>
     </form>
   );
 };
